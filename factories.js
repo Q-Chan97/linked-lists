845b3723ba1fe9c2cc278b38db540bde816ce2b9
Quincy Hardin <61283652+Q-Chan69@users.noreply.github.com>
2025-12-23 13:21:18 -0600

    Create append method for LinkedList

--- factories.js
+++ factories.js
@@ -11,4 +11,17 @@ class LinkedList {
         this.tail = null;
         this.size = 0;
     }
+
+    append(value) {
+        let newNode = new Node(value);
+
+        if (this.size === 0) {
+            this.head = newNode; // Both head and tail are the same node in a new list
+            this.tail = newNode;
+        } else {
+            this.tail.nextNode = newNode; // Connect previous tail
+            this.tail = newNode; // Actually adds new node as tail
+        }
+        this.size++; // Increase list size
+    }
 }
