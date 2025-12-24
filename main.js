baff9e061b2bab0d70adbe0f9d3c8e228663d4b3
Quincy Hardin <61283652+Q-Chan69@users.noreply.github.com>
2025-12-23 23:28:14 -0600

    Add more nodes

--- main.js
+++ main.js
@@ -3,5 +3,8 @@ import { LinkedList } from "./factories.js";
 let list = new LinkedList();
 
 list.append("Dog");
+list.append("Cat");
+list.append("Bird");
+list.append("Fish");
 
 console.log(list);
