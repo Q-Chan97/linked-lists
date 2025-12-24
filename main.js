b96942cf0b413ad30712fba711e33b6fac8bcc37
Quincy Hardin <61283652+Q-Chan69@users.noreply.github.com>
2025-12-24 09:07:37 -0600

    Create list for pop method example

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ import { LinkedList } from "./factories.js";
 
 let list = new LinkedList();
 
+let popList = new LinkedList();
+
 list.append("Dog");
 list.append("Cat");
 list.append("Bird");
@@ -11,3 +13,10 @@ list.prepend("Hamster"); // Should be first in list
 
 console.log(list.toString());
 console.log(list.at(1)); // Should be Dog
+
+popList.append("Burger King");
+popList.append("Wendy's");
+popList.append("McDonald's");
+
+console.log(popList.pop()); // Should return McDonald's
+console.log(popList.toString()); // Should return Burger King -> Wendy's -> null
